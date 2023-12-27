89bab845c65a33f6b3de454b325cde2d37287184
Sunil Kumar Saini <sunilkumarsaini6363@gmail.com>
2023-12-27 16:04:28 +0530

    Add new files

---
+++ 01_basics/datatypes.js
@@ -0,0 +1,6 @@
+// alert(2+3);            // we are using nodejs, not browser, this will show error
+console.log(typeof null) // object
+console.log(typeof undefined) // undefined
+
+
+// symbol => unique
